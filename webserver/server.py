eadf05bdfafcefe2ed76b5f54aa452169a81a845
BuffaloWill <wvandevanter@gmail.com>
2017-07-19 15:08:52 -0400

    switch to mongodb

--- webserver/server.py
+++ webserver/server.py
@@ -1,29 +1,27 @@
 from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
 import argparse
 import os
 import sys
-from git import *
 import requests
 import string 
 import json
+import re
 from os import listdir
 from os.path import isfile, join
-import re
+from bson import Binary, Code
+from bson.json_util import dumps
+
 
 dira = ""
 json_l = []
 
-# this loads all json files into memory, /me ducks
-def load_projects():
-	only = [f for f in listdir(dira) if isfile(join(dira, f))]
-	for file in only:
-		if "json" in file:
-			x = json.load(open(dira+file))
-			for q in x:
-				json_l.append(json.dumps(q))
-	print "Findings loaded: "+str(len(json_l))
-
 app = Flask(__name__)
+
+# initialize the db, allow user specficied later
+app.config['MONGO_URI'] = 'mongodb://127.0.0.1:27017/gumbler'
+mongo = PyMongo(app, config_prefix='MONGO')
+
 @app.route("/")
 def main():
 	return render_template('index.html')
@@ -33,112 +31,84 @@ def is_ascii(s):
     return all(ord(c) < 128 for c in s)
 
 def projects():
-	projects = set()
-	for datax in json_l:
-		data = json.loads(datax)
-		if len(data) > 0:
-			projects.add(data["project"])
-	return render_template('project.html',projects=projects)
-
-def org():
-	projects = set()
-	org = request.args.get("org")
-	for datax in json_l:
-		data = json.loads(datax)
-		if len(data) > 0:
-			if org in data["project"]:
-				projects.add(data["project"])
+	projects = mongo.db.findings.distinct("project")
 	return render_template('project.html',projects=projects)
 
 def project():
-	projects = []
-	project = request.args.get("project")
-	for datax in json_l:
-		data = json.loads(datax)
-		if len(data) > 0:
-			if data["project"] == project:
-				if data["results"] == "NOT DOWNLOADED":
-					data["not_downloaded"] = True
-				if is_ascii(data["results"]):
-					data["is_ascii"] = True
-					data["results"] = data["results"].replace("<","&lt;").replace(">","&gt;")
-				projects.append(data)
-	return render_template('display.html',projects=projects)
+	proj = request.args.get("project")
+	# do we need sanitization considerations?
+	projects = mongo.db.findings.find({"project":proj})
+
+	# this should be refactored 
+	p = []
+	for project in projects:
+		if project["results"] == "NOT DOWNLOADED":
+				project["not_downloaded"] = True
+		if is_ascii(project["results"]):
+			project["is_ascii"] = True
+			project["results"] = project["results"].replace("<","&lt;").replace(">","&gt;")
+		p.append(project)
+	return render_template('display.html',projects=p)
+
 
 def files():
-	projects = []
 	file = request.args.get("file")
+	projects = mongo.db.findings.find({"file":file})
 
-	for datax in json_l:
-		data = json.loads(datax)
-		if len(data) > 0:
-			if 'file' in data and file in data["file"]:
-				if data["results"] == "NOT DOWNLOADED":
-					data["not_downloaded"] = True
-				if is_ascii(data["results"]):
-					data["is_ascii"] = True
-					data["results"] = data["results"].replace("<","&lt;").replace(">","&gt;")
-				projects.append(data)
-	return render_template('file_list.html',projects=projects)
+	# this should be refactored 
+	p = []
+	for project in projects:
+		if project["results"] == "NOT DOWNLOADED":
+			project["not_downloaded"] = True
+		if is_ascii(project["results"]):
+			project["is_ascii"] = True
+			project["results"] = project["results"].replace("<","&lt;").replace(">","&gt;")
+			p.append(project)
+	return render_template('file_list.html',projects=p)
 
-def display():
-	projects = []
-	fname = request.args.get("filename")
-	for datax in json_l:
-		data = json.loads(datax)
-		if len(data) > 0:
-			if 'file' in data and fname == data["file"]:
-				if data["results"] == "NOT DOWNLOADED":
-					data["not_downloaded"] = True
-				if is_ascii(data["results"]):
-					data["is_ascii"] = True
-					data["results"] = data["results"].replace("<","&lt;").replace(">","&gt;")
-				projects.append(data)
-	return render_template('display.html',projects=projects)
-
-# these are common checks to look for in input
 def check():
-	txt_checks = []
-	for filename in os.listdir(os.getcwd()+"/webserver/checks"):
-		data = json.load(open(os.getcwd()+"/webserver/checks/"+filename))
-		data["filename"] = filename
-		txt_checks.append(data)
+	txt_checks = mongo.db.checks.find()
 	return render_template('checks.html', txt_checks=txt_checks)
 
 # run a list of regexes across all loaded projects
-def run_regex(regexes):
+def run_regex(regexes,matching_only):
 	results = []
-	for datax in json_l:
-		data = json.loads(datax)
-		if len(data) > 0:
-			for regex in regexes:
-				reg = re.compile(regex)
-				match = reg.search(data["results"])
-				if (not ("Error pulling file" in data["results"]) and match):					
-					if data["results"] == "NOT DOWNLOADED":
-						data["not_downloaded"] = True
-					if is_ascii(data["results"]):
-						data["is_ascii"] = True
-						data["results"] = data["results"].replace("<","&lt;").replace(">","&gt;")
-					data["match"] = match.group(0)
-					results.append(data)
+
+	for regex in regexes:
+		reg = re.compile(regex)
+		findings = mongo.db.findings.find({"results":reg})
+		for finding in findings:
+			if not ("Error pulling file" in finding["results"]):
+				if finding["results"] == "NOT DOWNLOADED":
+					finding["not_downloaded"] = True
+				if is_ascii(finding["results"]):
+					finding["is_ascii"] = True
+					finding["results"] = finding["results"].replace("<","&lt;").replace(">","&gt;")
+
+				if matching_only:
+					# show only the matching text
+					match = reg.search(finding["results"])
+					finding["match"] = finding.group(0)
+				else:
+					finding["match"] = finding["results"]
+
+				finding["regex"] = regex
+				results.append(finding)
 	return results
 
 # run the check provided
 def run_check():
 	check = request.args.get("check")
+	matching_only = request.args.get("matching_only")
+
 	results = []
 
-	for filename in os.listdir(os.getcwd()+"/webserver/checks"):
-		# this is so ugly, but better than reading files off the os
-		if filename == check:
-			# load the check
-			data = json.load(open(os.getcwd()+"/webserver/checks/"+check))
-			regexes = data["check_regex"]
-			results = run_regex(regexes)
-	print "|+| Hits found:"+str(len(results))
-	return render_template('check_results.html', results=results)
+	txt_check = mongo.db.checks.find_one({"name":check})
+	regexes = txt_check["check_regex"]
 
+	results = run_regex(regexes,matching_only)
+	print("|+| Hits found:"+str(len(results)))
+	return render_template('check_results.html', results=results)
 
 # list all projects from json e.g. /list
 app.add_url_rule('/list', 'index', projects)
@@ -146,14 +116,8 @@ app.add_url_rule('/list', 'index', projects)
 # list all files matching name e.g. /files?file=gemfile
 app.add_url_rule('/files', 'files', files)
 
-# display a filename /display?filename=[filename]
-app.add_url_rule('/display', 'display', display)
-
 # display a project /project?project=x
 app.add_url_rule('/project', 'project', project)
-
-# display a project /org?org=x
-app.add_url_rule('/org', 'org', org)
 
 # index
 app.add_url_rule('/index', 'main', main)
